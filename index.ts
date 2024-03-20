1865ab07f4820aeeea7092eff933588bf4c34a8f
AmirShafiqueWR <amirwr92@gmail.com>
2024-03-20 16:54:23 +0500

    My first Commit for push to GitHub

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+// SHABANG
+
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "firstNumber" },
+  { message: "Enter Second Number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction","Multiplication", "Division"]
+  },
+]);
+var result:number= 0;
+switch(answer.operator){
+    case "Addition":
+        result = answer.firstNumber + answer.secondNumber;
+        break;
+    case "Subtraction":
+        result = answer.firstNumber - answer.secondNumber;
+        break;
+    case "Multiplication":
+        result = answer.firstNumber * answer.secondNumber;
+        break;
+    case "Division":
+        result = answer.firstNumber / answer.secondNumber;
+        break;
+            
+    
+}
+console.log(result);
